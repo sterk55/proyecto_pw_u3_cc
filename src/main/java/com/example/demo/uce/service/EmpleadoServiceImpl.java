8621693519c0bc8e8314455b330e73903687b8fe
Cristhian Cedeño <daniel49571@gmail.com>
2022-08-17 19:49:59 -0500

    Taller24Nuevo

--- src/main/java/com/example/demo/uce/service/EmpleadoServiceImpl.java
+++ src/main/java/com/example/demo/uce/service/EmpleadoServiceImpl.java
@@ -20,12 +20,14 @@ public class EmpleadoServiceImpl implements IEmpleadoService{
 	@Override
 	public void crear(Empleado empleado) {
 		// TODO Auto-generated method stub
+		this.empleadoRepository.crear(empleado);
 		
 	}
 
 	@Override
 	public void actualizar(Empleado empleado) {
 		// TODO Auto-generated method stub
+		this.empleadoRepository.actualizar(empleado);
 		
 	}
 
@@ -33,12 +35,14 @@ public class EmpleadoServiceImpl implements IEmpleadoService{
 	public void eliminar(Integer id) {
 		// TODO Auto-generated method stub
 		
+		this.empleadoRepository.eliminar(id);
+		
 	}
 
 	@Override
 	public Empleado buscarPorId(Integer id) {
 		// TODO Auto-generated method stub
-		return null;
+		return this.empleadoRepository.buscar(id);
 	}
 
 }
